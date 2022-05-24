0e028f8038c8680cf9ed7b59de70956b8df155f6
sivanmiroglu <sivanmiroglu854@gmail.com>
2022-05-24 23:52:41 +0300

    Entity Framework Student Example First Step

---
+++ SQLEntityStudentexample.sql
@@ -0,0 +1,22 @@
+﻿Create Table Tbl_Student(
+ID int PRIMARY Key IDENTITY (1,1),
+FirstName varchar(20),
+LastName varchar(20),
+Picture varchar(100) 
+)
+
+Create Table Tbl_Lesson(
+LESSONID int PRIMARY KEY IDENTITY (1,1),
+LessonName varchar(20)
+)
+
+Create Table Tbl_Notes(
+NOTEID int PRIMARY KEY IDENTITY (1,1),
+Student int,
+Lesson int,
+EXAM1 smallint,
+EXAM2 smallint,
+EXAM3 smallint,
+AVERAGE decimal(5,2),
+SİTUATİON bit
+)
